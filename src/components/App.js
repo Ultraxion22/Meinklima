9744ef7ccfdf341e627fdcfd452acac1664cb920
Ultraxion22 <antonio.vargas_a@mail.udp.cl>
2021-07-27 20:41:55 -0400

    save11

--- src/components/App.js
+++ src/components/App.js
@@ -32,6 +32,7 @@ function App() {
                         <Route path="/proyectos" exact component={Proyecto}></Route>
                         <Route path="/catalogo" exact component={Catalogo}></Route>
                     </Switch>
+                    <Fotter />
         
 
                     
