f39d2817b1a3b7f96c8d0e4cfa91e73f6bc8cfe4
Dante Toro <avilesdante59@gmail.com>
2021-09-05 23:26:17 -0300

    gg

--- src/views/Home.js
+++ src/views/Home.js
@@ -27,7 +27,6 @@ export default function Home() {
                             <h1 className="p-5 pt-3 m-0 d-lg-block d-none">Lo que te ofrecemos a alcance de un click
                             Lo que te ofrecemos a alcance de un click</h1>
                         </div>
-<<<<<<< HEAD
                         <div class="col col-md-6 posters" style={{backgroundColor:"#D60238"}}>
                             <h2 style={{color:"#fff"}}>¿Necesitas asistencia técnica en tu hogar?</h2>
                             <div id="carouselExampleDark" class="carousel carousel-dark slide m-auto" data-bs-ride="carousel">
@@ -69,36 +68,6 @@ export default function Home() {
                                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                     <span class="visually-hidden">Next</span>
                                 </button>
-=======
-                        <div class="col-lg-4 p-3 posters" style={{backgroundColor:"#D60238"}}>
-                            <h2 className="pt-5 pe-5 ps-5"style={{color:"#fff"}}>¿Necesitas asistencia técnica en tu hogar?</h2>
-                            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-                            <div class="carousel-indicators">
-                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                            </div>
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                    <div className="container-fluid h-100"><h1 className="p-5 mt-5 mb-5 text-center">SERVICIOS A LA MEDIDA DE SU HOGAR</h1></div>
-                                </div>
-                                <div class="carousel-item">
-                                <div className="container-fluid h-100"><h1 className="p-5 mt-5 mb-5 text-center">SERVICIOS A LA MEDIDA DE SU HOGAR</h1></div>
-
-                                </div>
-                                <div class="carousel-item">
-                                <div className="container-fluid h-100"><h1 className="p-5 mt-5 mb-5 text-center">SERVICIOS A LA MEDIDA DE SU HOGAR</h1></div>
-                                </div>
-                            </div>
-                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
-                            </button>
-                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
-                            </button>
->>>>>>> d58488cdcedcca307f3cea9268f2272cc6bc8881
                             </div>
                         </div>
                     </div>
