090df9c5b92ec8ba0f1542c80bf73d0e8c12c2f5
Ultraxion22 <antonio.vargas_a@mail.udp.cl>
2021-10-08 00:06:37 -0300

    commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const bodyParser = require ('body-parser')
+const cors = require("cors");
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "meinklimacatalogo"
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/api/get", (req, res) => {
+    const sqlSelect = "SELECT * FROM calderas";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    });
+});
+
+
+app.listen(3001, () =>{
+    console.log("running on port 3001");
+});
